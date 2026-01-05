fd30b5864b5d2bc94b2bb74ccf2410dc09731a73
root <root@Obunde-78432>
2026-01-05 06:03:42 +0300

    corrected the assosiations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :shop
   has_many :order_foods
+  has_many :orders, through: :order_foods
 
   # STI (Single Table Inheritance) setup
   self.inheritance_column = :type
